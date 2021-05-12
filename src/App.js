f35973bb6e5380079198494ae39e146d15b93286
Abhiram Satpute <abhiram.satpute@gmail.com>
2021-05-12 21:35:16 +0530

    Added header with css: avatar, search box and help Material UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
-    <div className="App">
+    // BEM naming convention
+    <div className="app">
       <h1>Starting a slack clone!! 🚀</h1>
+      <br />
+
+      {/* Header = search bar at top */}
+      {/* Sidebar = profile, channels, drafts, saved itmes, etc...*/}
+      {/* React Router at end for chat screen */}
+      <Header></Header>
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Header.css";
+import { Avatar } from "@material-ui/core";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import SearchIcon from "@material-ui/icons/Search";
+import HelpOutlineIcon from "@material-ui/icons/HelpOutline";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <Avatar
+          className="header__avatar"
+          alt="Abhiram Satpute" //{user?.displayName}
+          src="" //{user?.photoURL}
+        />
+        <AccessTimeIcon />
+        {/* <h1>left side of header</h1> */}
+        {/* time icon, avatar as well? */}
+      </div>
+      <div className="header__search">
+        {/* <h1>center of header</h1> */}
+        {/* search, input */}
+        <SearchIcon />
+        <input placeholder="Search in Abhi's Slack Dome" />
+      </div>
+      <div className="header__right">
+        <HelpOutlineIcon />
+        {/* <h1>right side of header</h1> */}
+      </div>
+    </div>
+  );
+}
+
+export default Header;
