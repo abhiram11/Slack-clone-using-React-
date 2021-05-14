f2ba6d6a42b93b0f2a258c0588056e55549b233f
Abhiram Satpute <abhiram.satpute@gmail.com>
2021-05-15 03:28:01 +0530

    updated chat integration

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import Chat from "./Chat";
 
 // 6:31:48 MaterialUI icons are FONTS
 // 6:34:30 nested CSS children!!
@@ -12,23 +14,40 @@ import Sidebar from "./Sidebar";
 // 6:59:00 what default export does...
 // 7:02:00 structure of firebase cloud and the databases in general
 // 7:05:20 everytime a user sends a message we get the data in {field (type, value)}:
-//  {username (string, value), userimage (string, value of url), message (string, value), timestamp (timestamp)}
+// {username (string, value), userimage (string, value of url), message (string, value), timestamp (timestamp)}
 // 7:06:50 useEffect (()=>{}, []) last sq brackets we call them dependencies
+// 7:21:00 starting with REACT ROUTER, CHUCK ALL THE APP INSIDE <ROUTINE>
+// 7:25:00 intro to useParams from ReactRouter
 
 function App() {
   return (
     // BEM naming convention
     <div className="app">
-      <h1>Starting a slack clone!! 🚀</h1>
-      <br />
+      <Router>
+        {/* <h1>Starting a slack clone!! 🚀</h1> */}
+        {/* <br /> */}
 
-      {/* Header = search bar at top */}
-      {/* Sidebar = profile, channels, drafts, saved itmes, etc...*/}
-      {/* React Router at end for chat screen */}
-      <Header />
-      <div className="app__body">
-        <Sidebar />
-      </div>
+        {/* Header = search bar at top */}
+        {/* Sidebar = profile, channels, drafts, saved itmes, etc...*/}
+        {/* React Router at end for chat screen */}
+        <Header />
+        <div className="app__body">
+          <Sidebar />
+
+          <Switch>
+            <Route path="/room/:roomId">
+              {" "}
+              {/*The syntax is important here!! */}
+              {/* <h1>CHAT SCREEN</h1> */}
+              <Chat />
+            </Route>
+            <Route path="/">
+              {/* <h1>WELCOME to home screen</h1> */}
+              {/* <Chat /> */}
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
